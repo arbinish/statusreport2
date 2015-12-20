66efff4a07760b283ef8ec72532d9f7d25a6184b
Binish A R <arbinish@gmail.com>
2015-12-21 05:00:41 +0530

    persist in localStorage

--- js/StatusList.js
+++ js/StatusList.js
@@ -5,20 +5,23 @@ var Status = require('./Status.js');
 
 var StatusList = React.createClass({
 	getInitialState: function() {
-		return {
-			status: [{
-				date: 1448908200000,
-				text: "Capacity uplift of prod application"
-			},
-			{
-				date: 1448994600000,
-				text: "Version 2.0 rolled out in production"
-			},
-			{
-				date: 1449081000000,
-				text: "Database maintenance"
-			}]
+		var status = localStorage.getItem("status");
+		if (status == null) {
+			localStorage.setItem("status", JSON.stringify(
+				[{
+					date: 1448908200000,
+					text: "Capacity uplift of prod application"
+				},
+				{
+					date: 1448994600000,
+					text: "Version 2.0 rolled out in production"
+				},
+				{
+					date: 1449081000000,
+					text: "Database maintenance"
+				}]));
 		}
+		return {status: JSON.parse(localStorage.getItem("status"))};
 	},
 	update: function(index, content) {
 		var status = this.state.status;
@@ -26,20 +29,23 @@ var StatusList = React.createClass({
 			date: (new Date()).getTime(),
 			text: content
 		};
-
 		this.setState({status: status});
-
+		this.persistState();
 	},
 	deleteEntry: function(index) {
 		var status = this.state.status;
 		console.log("Deleting", status[index], " index", index);
 		status.splice(index, 1);
 		this.setState({status: status});
+		this.persistState();
 	},
 	createStatus: function(status, index) {
 		return (
 			<Status key={index} content={status.text} date={status.date}     pos={index} updateStatus={this.update} deleteStatus={this.deleteEntry}/>
 			)
+	},
+	persistState: function() {
+		localStorage.setItem("status", JSON.stringify(this.state.status));
 	},
 	add: function() {
 		var now = (new Date()).getTime(),
@@ -47,8 +53,9 @@ var StatusList = React.createClass({
 			this.state.status.push({
 				date: now,
 				text: content
-			});
+			});		
 		this.setState({status: this.state.status});
+		this.persistState();
 	},
 	render: function() {
 		return (
